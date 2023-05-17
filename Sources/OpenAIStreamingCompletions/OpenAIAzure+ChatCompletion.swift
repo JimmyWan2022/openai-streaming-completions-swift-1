856eaaf414760b7b4ab05888ef310f4d619926e4
JimmyWan2022 <jimmywan2023@gmail.com>
2023-05-17 22:23:03 +0800

    提取出内容 公共部分

--- Sources/OpenAIStreamingCompletions/OpenAIAzure+ChatCompletion.swift
+++ Sources/OpenAIStreamingCompletions/OpenAIAzure+ChatCompletion.swift
@@ -8,47 +8,47 @@
 import Foundation
 
 extension OpenAIAzure {
-    public struct Message: Equatable, Codable, Hashable {
-        public enum Role: String, Equatable, Codable, Hashable {
-            case system
-            case user
-            case assistant
-        }
-
-        public var role: Role
-        public var content: String
-
-        public init(role: Role, content: String) {
-            self.role = role
-            self.content = content
-        }
-    }
-
-    public struct ChatCompletionRequest: Codable {
-        var messages: [Message]
-        var model: String
-        var max_tokens: Int = 1500
-        var temperature: Double = 0.2
-        var stream = false
-        var stop: [String]?
-
-        public init(messages: [Message], model: String = "gpt-3.5-turbo", max_tokens: Int = 1500, temperature: Double = 0.2, stop: [String]? = nil) {
-            self.messages = messages
-            self.model = model
-            self.max_tokens = max_tokens
-            self.temperature = temperature
-            self.stop = stop
-        }
-    }
+//    public struct Message: Equatable, Codable, Hashable {
+//        public enum Role: String, Equatable, Codable, Hashable {
+//            case system
+//            case user
+//            case assistant
+//        }
+//
+//        public var role: Role
+//        public var content: String
+//
+//        public init(role: Role, content: String) {
+//            self.role = role
+//            self.content = content
+//        }
+//    }
+//
+//    public struct ChatCompletionRequest: Codable {
+//        var messages: [Message]
+//        var model: String
+//        var max_tokens: Int = 1500
+//        var temperature: Double = 0.2
+//        var stream = false
+//        var stop: [String]?
+//
+//        public init(messages: [Message], model: String = "gpt-3.5-turbo", max_tokens: Int = 1500, temperature: Double = 0.2, stop: [String]? = nil) {
+//            self.messages = messages
+//            self.model = model
+//            self.max_tokens = max_tokens
+//            self.temperature = temperature
+//            self.stop = stop
+//        }
+//    }
 
     // MARK: - Plain completion
 
-    struct ChatCompletionResponse: Codable {
-        struct Choice: Codable {
-            var message: Message
-        }
-        var choices: [Choice]
-    }
+//    struct ChatCompletionResponse: Codable {
+//        struct Choice: Codable {
+//            var message: Message
+//        }
+//        var choices: [Choice]
+//    }
 
     public func completeChat(_ completionRequest: ChatCompletionRequest) async throws -> String {
         let request = try createChatRequestAzure(completionRequest: completionRequest,ip: "localhost",port: "9091")
@@ -147,16 +147,16 @@ extension OpenAIAzure {
         return completion
     }
 
-    private struct ChatCompletionStreamingResponse: Codable {
-        struct Choice: Codable {
-            struct MessageDelta: Codable {
-                var role: Message.Role?
-                var content: String?
-            }
-            var delta: MessageDelta
-        }
-        var choices: [Choice]
-    }
+//    private struct ChatCompletionStreamingResponse: Codable {
+//        struct Choice: Codable {
+//            struct MessageDelta: Codable {
+//                var role: Message.Role?
+//                var content: String?
+//            }
+//            var delta: MessageDelta
+//        }
+//        var choices: [Choice]
+//    }
 
     private func decodeChatStreamingResponse(jsonStr: String) -> String? {
         guard let json = try? JSONDecoder().decode(ChatCompletionStreamingResponse.self, from: Data(jsonStr.utf8)) else {
